9b810cd1745b8a9e9e1cb3a197b11e33aac7f6f4
Steven Hurwitt <stevenhurwitt@gmail.com>
2019-09-02 08:53:16 -0500

    .

--- IDR_Drop/EPOwebscrape.py
+++ IDR_Drop/EPOwebscrape.py
@@ -76,7 +76,13 @@ def logon(username, pw, ngrid):
     opts.add_argument('--no-sandbox')
     opts.add_argument('--disable-dev-shm-usage')
     opts.binary_location = '/usr/bin/google-chrome'
-    prefs ={"profile.default_content_settings.popups": 0, "download.default_directory": "/home/jupyter-engiela/la-tools-test/IDR_Drop/Downloads/", "directory_upgrade": True}
+    prefs = {
+                'download.default_directory': '/home/jupyter-engiela/la-tools-test/IDR_Drop/Downloads',
+                'download.prompt_for_download': False,
+                'download.directory_upgrade': True,
+                'safebrowsing.enabled': False,
+                'safebrowsing.disable_download_protection': True}
+    #prefs ={"profile.default_content_settings.popups": 0, "download.default_directory": "/home/jupyter-engiela/la-tools-test/IDR_Drop/Downloads/", "directory_upgrade": True}
     opts.add_experimental_option("prefs", prefs)
     assert opts.headless
 
