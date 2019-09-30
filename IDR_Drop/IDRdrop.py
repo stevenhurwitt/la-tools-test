d6c9cb498af531b5bebeaa3a83d7d093828861e5
Steven Hurwitt <steven.hurwitt@external.engie.com>
2019-09-30 11:29:00 -0500

    update

--- IDR_Drop/IDRdrop.py
+++ IDR_Drop/IDRdrop.py
@@ -40,7 +40,7 @@ def raw_split(filedf, readdir, writedir, utility):
         sub = filedf.loc[filedf.Account == name,:].reset_index(drop = True)
         
         ldc = str(name).split(' ')[0]
-        acct_id = '_'.join([ldc, utility])
+        acct_id = '_'.join([utility, ldc])
         
         write_name = ''.join([acct_id, "_IDR_RAW.csv"])
         
