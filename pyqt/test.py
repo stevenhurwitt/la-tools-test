ad8f3a3122cea2d02b71ed4d3fb3747193806ecc
Steven Hurwitt <stevenhurwitt@gmail.com>
2020-01-19 18:17:16 -0500

    Add files via upload

--- pyqt/test.py
+++ pyqt/test.py
@@ -32,16 +32,9 @@ class App(QWidget):
        # Create table
         self.tableWidget = QTableWidget()
         self.tableWidget.setRowCount(4)
-        self.tableWidget.setColumnCount(2)
-        self.tableWidget.setItem(0,0, QTableWidgetItem("Cell (1,1)"))
-        self.tableWidget.setItem(0,1, QTableWidgetItem("Cell (1,2)"))
-        self.tableWidget.setItem(1,0, QTableWidgetItem("Cell (2,1)"))
-        self.tableWidget.setItem(1,1, QTableWidgetItem("Cell (2,2)"))
-        self.tableWidget.setItem(2,0, QTableWidgetItem("Cell (3,1)"))
-        self.tableWidget.setItem(2,1, QTableWidgetItem("Cell (3,2)"))
-        self.tableWidget.setItem(3,0, QTableWidgetItem("Cell (4,1)"))
-        self.tableWidget.setItem(3,1, QTableWidgetItem("Cell (4,2)"))
-        self.tableWidget.move(0,0)
+        self.tableWidget.setColumnCount(3)
+        data1 = ['row1', 'row2', 'row3', 'row4']
+        data2 = [1, 2, 3, 4]
 
         # table selection change
         for index in range(4):
